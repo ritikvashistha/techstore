9c4e5df81257eb7579dcc72652a0bc65bc3b173e
ritikvashistha <vashistharitik9@gmail.com>
2020-07-02 17:18:46 +0530

    Updates

---
+++ precache-manifest.6b9bb2a077319e54d98e543a2695c518.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f295f81ff638a78e98df73ec1b58c8",
+    "url": "/techstore/index.html"
+  },
+  {
+    "revision": "acb65bd936de9731c43b",
+    "url": "/techstore/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7a7fe4802602f75d9355",
+    "url": "/techstore/static/css/main.404fed0c.chunk.css"
+  },
+  {
+    "revision": "acb65bd936de9731c43b",
+    "url": "/techstore/static/js/2.9af8cc02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/techstore/static/js/2.9af8cc02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7fe4802602f75d9355",
+    "url": "/techstore/static/js/main.73d63321.chunk.js"
+  },
+  {
+    "revision": "55b19e85e061567ad200",
+    "url": "/techstore/static/js/runtime-main.bafbdd46.js"
+  },
+  {
+    "revision": "c712a03ea07a1e49b1eb2de0252f0bc0",
+    "url": "/techstore/static/media/aboutBcg.c712a03e.jpg"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/techstore/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/techstore/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/techstore/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "a84e8cd8b849c8605fee86a31a2dda32",
+    "url": "/techstore/static/media/mainBcg.a84e8cd8.jpg"
+  },
+  {
+    "revision": "33f1346302f2fa130eba7051d40e1c6b",
+    "url": "/techstore/static/media/productsBcg.33f13463.jpg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/techstore/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/techstore/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
